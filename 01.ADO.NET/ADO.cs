62e7fe61d4de486a446d79e494b4ad0bd5d90135
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-10-25 23:43:38 +0300

    resolve with async

--- 01.ADO.NET/ADO.cs
+++ 01.ADO.NET/ADO.cs
@@ -14,85 +14,186 @@ namespace AdoNEt
             {
                 conncetion.Open();
 
-                string createDatabase = "CREATE DATABASE MinionsDB";
-                ExecutionNonQuery(conncetion, createDatabase);
-                string[] createTable = CreateTableMethod();
-                foreach (var currentLine in createTable)
-                {
-                    ExecutionNonQuery(conncetion, currentLine);
-                }
-                string[] insertDataStatments = InsertValuesToMinionsDBTables();
+                //string createDatabase = "CREATE DATABASE MinionsDB";
+                //ExecutionNonQuery(conncetion, createDatabase);
+                //string[] createTable = CreateTableMethod();
+                //foreach (var currentLine in createTable)
+                //{
+                //    ExecutionNonQuery(conncetion, currentLine);
+                //}
+                //string[] insertDataStatments = InsertValuesToMinionsDBTables();
 
-                foreach (var currentInsert in insertDataStatments)
-                {
-                    ExecutionNonQuery(conncetion, currentInsert);
-                    ;
-                }
+                //foreach (var currentInsert in insertDataStatments)
+                //{
+                //    ExecutionNonQuery(conncetion, currentInsert);
+                //    ;
+                //}
 
-                string quary = @"SELECT [Name],COUNT(mv.MinionId) FROM Villains  AS v JOIN MinionsVillains AS mv ON mv.VillainId=v.Id GROUP BY v.Id, v.[Name]";
-                using (SqlCommand command = new SqlCommand(quary, conncetion))
-                {
-                    using (var reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
+                //string quary = @"SELECT [Name],COUNT(mv.MinionId) FROM Villains  AS v JOIN MinionsVillains AS mv ON mv.VillainId=v.Id GROUP BY v.Id, v.[Name]";
+                //using (SqlCommand command = new SqlCommand(quary, conncetion))
+                //{
+                //    using (var reader = command.ExecuteReader())
+                //    {
+                //        while (reader.Read())
+                //        {
 
-                            string name = (string)reader[0];
-                            int count = (int)reader[1];
-                            Console.WriteLine(name + " - " + count);
+                //            string name = (string)reader[0];
+                //            int count = (int)reader[1];
+                //            Console.WriteLine(name + " - " + count);
 
-                            ;
-                        }
+                //            ;
+                //        }
 
-                    }
-                }
+                //    }
+                //}
+                //int inputId = int.Parse(Console.ReadLine());
+                //string quaryId = $@"SELECT Name FROM Villains WHERE Id = @Id";
+                //using (SqlCommand command = new SqlCommand(quaryId, conncetion))
+                //{
+                //    StringBuilder sb = new StringBuilder();
+                //    command.Parameters.AddWithValue("@Id", inputId);
+                //    var result = command.ExecuteScalar();
+                //   string resultString = SelectMethod(result,inputId,conncetion,sb);
+                //    Console.WriteLine(resultString);
+                //}
+
+
+                //SELECT Id FROM Villains WHERE Name = @Name
+                //SELECT Id FROM Minions WHERE Name = @Name
+                //INSERT INTO MinionsVillains(MinionId, VillainId) VALUES(@villainId, @minionId)
+                //INSERT INTO Villains(Name, EvilnessFactorId)  VALUES(@villainName, 4)
+                //INSERT INTO Minions(Name, Age, TownId) VALUES(@nam, @age, @townId)
+                //INSERT INTO Towns(Name) VALUES(@townName)
+                //SELECT Id FROM Towns WHERE Name = @townName
                 StringBuilder sb = new StringBuilder();
-                int inputId = int.Parse(Console.ReadLine());
-                string quaryId = $@"SELECT Name FROM Villains WHERE Id = @Id";
-                using (SqlCommand command = new SqlCommand(quaryId, conncetion))
+                Console.Write("Minion: ");
+                string[] minions = Console.ReadLine().Split(" ");
+                Console.Write("Villain: ");
+                string villain = Console.ReadLine();
+                string villainName = villain;
+                string minionName = minions[0];
+                int age = int.Parse(minions[1]);
+                string townName = minions[2];
+                var iDVillain = new object();
+                var iDMinions = new object();
+                var iDTown = new object();
+
+                string townId = @"SELECT Id FROM Towns WHERE Name = @townName";
+                using (SqlCommand command = new SqlCommand(townId, conncetion))
                 {
-                    command.Parameters.AddWithValue("@Id", inputId);
-                    var result = command.ExecuteScalar();
-                    if (result == null)
+                    command.Parameters.AddWithValue("@townName", townName);
+                    iDTown = command.ExecuteScalar();
+                    if (iDTown == null)
                     {
-                        sb.AppendLine($"No villain with ID {inputId} exists in the database.");
-                    }
-                    else
-                    {
-                        sb.AppendLine($"Villain: {result}");
-                        string quaryMinions = $@"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,    m.Name,   m.Age  FROM MinionsVillains AS mv  JOIN Minions As m ON mv.MinionId = m.Id  WHERE mv.VillainId = @Id ORDER BY m.Name";
-                        using (SqlCommand commandMinions = new SqlCommand(quaryMinions, conncetion))
+
+                        string townQuarry = @"INSERT INTO Towns(Name) VALUES(@townName)";
+                        using (var townCommand = new SqlCommand(townQuarry, conncetion))
                         {
-                            commandMinions.Parameters.AddWithValue("@Id", inputId);
-                            using (var reader = commandMinions.ExecuteReader())
-                            {
-                                if (reader.HasRows)
-                                {
-                                   
-                                    sb.AppendLine("(no minions)");
-                                }
-                                else
-                                {
-                                    while (reader.Read())
-                                    {
-
-                                        var id = reader[0];
-                                        var name = reader[1];
-                                        var age = reader[2];
-                                        sb.AppendLine($"{id}. {name} {age}");
-                                    }
-                                }
-                            
-                            }
-
+                            townCommand.Parameters.AddWithValue("@townName", townName);
+                            townCommand.ExecuteNonQuery();
+                            sb.AppendLine($"Town {townName} was added to the database.");
                         }
 
                     }
-                    Console.WriteLine(sb.ToString());
+
                 }
+                string villainNameId = @"SELECT Id FROM Villains  WHERE Name = @Name";
+                using (SqlCommand command = new SqlCommand(villainNameId, conncetion))
+                {
+                    command.Parameters.AddWithValue("@Name", villainName);
+                    iDVillain = command.ExecuteScalar();
+                    if (iDVillain == null)
+                    {
+                        string villainQuarry = @"INSERT INTO Villains(Name, EvilnessFactorId)  VALUES(@villainName, 4)";
+                        using (var villainCommand = new SqlCommand(villainQuarry, conncetion))
+                        {
+                            villainCommand.Parameters.AddWithValue("@villainName", villainName);
+                            villainCommand.ExecuteNonQuery();
+                            sb.AppendLine($"Villain {villainName} was added to the database.");
+                        }
+                    }
+                }
+
+                string minionNameId = @"SELECT Id FROM Minions WHERE Name = @Name";
+                using (SqlCommand command = new SqlCommand(minionNameId, conncetion))
+                {
+                    command.Parameters.AddWithValue("@Name", minionName);
+                    iDMinions = command.ExecuteScalar();
+                    if (iDMinions == null)
+                    {
+                        string minionsQuarry = @"INSERT INTO Minions(Name, Age, TownId) VALUES(@name, @age, @townId)";
+                        using (var minionsCommand = new SqlCommand(minionsQuarry, conncetion))
+                        {
+
+                            minionsCommand.Parameters.AddWithValue("@name", minionName);
+                            minionsCommand.Parameters.AddWithValue("@age", age);
+                            minionsCommand.Parameters.AddWithValue("@townId", iDTown);
+                            minionsCommand.ExecuteNonQuery();
+
+                            sb.AppendLine($"Successfully added {minionName} to be minion of {villainName}.");
+                        }
+                    }
+                }
+                if(iDMinions!=null && iDVillain !=null)
+                {
+                    string quarry = @"INSERT INTO MinionsVillains(MinionId, VillainId) VALUES(@villainId, @minionId)";
+                    using (var command = new SqlCommand(quarry, conncetion))
+                    {
+                        command.Parameters.AddWithValue("@villainId", iDVillain);
+                        command.Parameters.AddWithValue("@minionId", iDMinions);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                Console.WriteLine(sb.ToString());
+            
             }
         }
 
+        private static string SelectMethod(object result, int inputId, SqlConnection conncetion, StringBuilder sb)
+        {
+            if (result == null)
+            {
+                sb.AppendLine($"No villain with ID {inputId} exists in the database.");
+            }
+            else
+            {
+                sb.AppendLine($"Villain: {result}");
+                string quaryMinions = $@"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,    m.Name,   m.Age  FROM MinionsVillains AS mv  JOIN Minions As m ON mv.MinionId = m.Id  WHERE mv.VillainId = @Id ORDER BY m.Name";
+                using (SqlCommand commandMinions = new SqlCommand(quaryMinions, conncetion))
+                {
+                    commandMinions.Parameters.AddWithValue("@Id", inputId);
+                    using (var reader = commandMinions.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+
+                            sb.AppendLine("(no minions)");
+                        }
+                        else
+                        {
+                            while (reader.Read())
+                            {
+
+                                var id = reader[0];
+                                var name = reader[1];
+                                var age = reader[2];
+                                sb.AppendLine($"{id}. {name} {age}");
+                            }
+                        }
+
+                    }
+
+                }
+
+            }
+            return sb.ToString().TrimEnd();
+        }
+        private static string InsertValues(string quarry)
+        {
+            string result = quarry;
+
+            return result;
+        }
         private static string[] InsertValuesToMinionsDBTables()
         {
             string[] result = new string[] {
---
+++ 01.ADO.NET/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace NETADO
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SqlConnection sqlConnection = new SqlConnection(Configuration.CONNECTION_STRING);
+
+           await sqlConnection.OpenAsync();
+
+           //await using(sqlConnection)
+           // {
+           //     await ViliansWIthMoreThan3MinionsAsync(sqlConnection);
+           // }
+
+           await using(sqlConnection)
+            {
+                await MinionNamesOnCurrentVillainAsync(sqlConnection);
+            }
+
+        }
+
+        private static async Task MinionNamesOnCurrentVillainAsync(SqlConnection sqlConnection)
+        {
+
+            SqlCommand idVillaincommand = new SqlCommand(Quarries.GET_ID_FROM_VILAINS, sqlConnection);
+           
+            SqlDataReader villainReader = await idVillaincommand.ExecuteScalarAsync();
+            string villainName = villainReader.GetString(0);
+            Console.WriteLine(villainName);
+
+        }
+
+        static private async Task ViliansWIthMoreThan3MinionsAsync(SqlConnection sqlconnetction)
+        {
+            SqlCommand command = new SqlCommand(Quarries.VILIANS_WITH_MORE_THAN_3_MINIONS,sqlconnetction);
+            SqlDataReader reader = await command.ExecuteReaderAsync();
+
+            await using (reader)
+            {
+                while (await reader.ReadAsync())
+                {
+
+                    string name = reader.GetString(0);
+                    int count = reader.GetInt32(1);
+                    Console.WriteLine($"{name} - {count}");
+                }
+            }
+        }
+    }
+}
