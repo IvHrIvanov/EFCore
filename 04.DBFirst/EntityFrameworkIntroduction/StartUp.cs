70634409ee9af03a0edd57db6ad9fe15cb7ccfdc
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-11-21 01:55:07 +0200

    4.Employees with Salary Over 50 000

--- 04.DBFirst/EntityFrameworkIntroduction/StartUp.cs
+++ 04.DBFirst/EntityFrameworkIntroduction/StartUp.cs
@@ -12,7 +12,7 @@ namespace SoftUni
         static void Main(string[] args)
         {
             SoftUniContext context = new SoftUniContext();
-            string employeesInfo = GetEmployeesFullInformation(context);
+            string employeesInfo = GetEmployeesWithSalaryOver50000(context);
             Console.WriteLine(employeesInfo);
         }
         public static string GetEmployeesFullInformation(SoftUniContext context)
@@ -25,9 +25,26 @@ namespace SoftUni
                     FullInfo = $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}"
                 })
                 .ToList();
-            foreach (var item in employees)
+            foreach (var employee in employees)
             {
-                sb.AppendLine(item.FullInfo);
+                sb.AppendLine(employee.FullInfo);
+            }
+            return sb.ToString().TrimEnd();
+        }
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e=>e.FirstName)
+                .Select(e => new
+                {
+                    EmployeeSalaryOver5000 = $"{e.FirstName} - {e.Salary:f2}"
+                }).ToList();
+            foreach (var emloyee in employees)
+            {
+                sb.AppendLine(emloyee.EmployeeSalaryOver5000);
             }
             return sb.ToString().TrimEnd();
         }
