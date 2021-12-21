27d736feda6ca1f8124211b444a1f8647f719b30
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-12-21 18:22:17 +0200

    Add

---
+++ 07.LINQ/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MusicHub.Data.Models.Enums
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [ForeignKey(nameof(Album))]
+        public int AlbumId { get; set; }
+        public Album Album { get; set; }
+
+        [Required]
+        public int WriterId { get; set; }
+
+        public string Writer { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+
+
+    }
+}
